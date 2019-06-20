7b7014edda6d0ab7c5e65f138c84a7d2cf4b3e7d
DNAthan42 <douglasnathan42@gmail.com>
2019-06-19 19:23:39 -0600

    Edit the target function to print the binary representation of the number of positive pixels. It's not great, loss converges to around .25

--- LearningTorch/LeNetImpl.py
+++ LearningTorch/LeNetImpl.py
@@ -20,7 +20,7 @@ class Net(nn.Module):
         # an affine operation: y = Wx + b
         self.fc1 = nn.Linear(16 * 6 * 6, 120)  # 6*6 from image dimension
         self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.fc3 = nn.Linear(84, 11)
 
     def forward(self, x):
         # Max pooling over a (2, 2) window
@@ -40,11 +40,21 @@ class Net(nn.Module):
             num_features *= s
         return num_features
 
-    def target(self, val):
-        # goal: if a 'pixel' is negative, classify -1, else 1
-        # [print(v.detach().numpy()[0], end='') for v in val]
-        # print()
-        return torch.tensor([[-1 if v.detach().numpy() < 0 else 1 for v in val[0]]], dtype=torch.float)
+    def target(self, net_in):
+
+        # return torch.tensor([[-1 if v.detach().numpy() < 0 else 1 for v in val[0]]], dtype=torch.float)
+        count = 0
+        net_in_formatted = net_in.detach().numpy()[0][0]
+
+        # count the number of non-negative pixels in the input.
+        # there's probably a more pythonic way to do this, but I doubt it would be readable
+        for row in net_in_formatted:
+            for col in row:
+                if col >= 0:
+                    count += 1
+
+        binary = format(count, '011b')
+        return torch.tensor([[int(digit) for digit in binary]], dtype=torch.float)
 
 
 stop = False
@@ -71,25 +81,31 @@ if __name__ == "__main__":
     try:
         while not stop:
             optimizer.zero_grad()
-            output = net(torch.randn(1, 1, 32, 32))  # Feed random input.
-            # print(f"output: {output}")
+            net_in = torch.randn(1,1,32,32)  # get random input
+
+            output = net(net_in)  # Feed random input.
+            target = net.target(net_in)  # calculate loss from given target function.
             # also, apparently '__call__' is a thing in python
 
-            target = net.target(output)  # calculate loss from given target function.
-            # print(f"target: {target}")
-            loss = criterion(output, target)
+            loss = criterion(output, target)  # evaluate loss
 
             loss_list.append(loss.detach().numpy())  # track all losses for eval at end
 
             loss.backward()
             optimizer.step()  # does the update, apparently.
 
-            count += 1
+            # Just making sure the function is still running
+            # count += 1
             if count % 1000 == 0:
                 print(count / 1000, end='')
+
+            count += 1
+            if count >= 5000:
+                stop = True
 
     except KeyboardInterrupt:
         pass
 
     # print(loss_list)
-    [print(v) for v in loss_list]
+    for v in range(0,len(loss_list)):
+        print(f"{v}, {loss_list[v]}")
